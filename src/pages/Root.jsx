28b1babed3475f43ccb7db14cce7c1e6adb94ddb
Serafim Kvashnin <serafim274715@gmail.com>
2024-09-18 22:37:37 +0300

    Use redirect instead of useNavigate

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,20 +1,18 @@
-import { useNavigate } from 'react-router-dom';
+import { redirect, useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection';
 import Navigation from '../components/navigation/Navigation';
 import { useEffect } from 'react';
 
 const Root = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const redirectTo = urlParams.get('?') || '/';
 
     if (redirectTo && redirectTo !== window.location.pathname) {
-      navigate(redirectTo, { replace: true });
+      redirect(redirectTo);
     }
-  }, [navigate]);
+  }, []);
 
   return (
     <div className="flex flex-col min-h-screen">
